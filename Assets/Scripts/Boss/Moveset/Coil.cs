2f9ba1caf09234405e332ebd60624bbe2359eaa8
ShiroKatake <tgkhang262@gmail.com>
2021-02-18 11:21:08 +1100

    ADD: Coiling move

---
+++ Assets/Scripts/Boss/Moveset/Coil.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coil : MonoBehaviour
+{
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float rotateSpeed;
+	[SerializeField] private float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Orbit();   
+    }
+
+	void Orbit()
+	{
+		transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+		transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+		transform.Translate(Vector3.up * pitch * Time.deltaTime);
+	}
+}
