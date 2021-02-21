a170f311c23b9d73aafa558d665063feb338c6ea
ShiroKatake <tgkhang262@gmail.com>
2021-02-21 17:27:22 +1100

    UPDATE: Added code to read csv file

--- Assets/Scripts/Boss/AbilityQueue.cs
+++ Assets/Scripts/Boss/AbilityQueue.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class AbilityQueue : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	//Private Fields---------------------------------------------------------------------------------------------------------------------------------
 
-    // Update is called once per frame
-    void Update()
+	//Serialized Fields----------------------------------------------------------------------------
+	private Queue<KeyValuePair<float, Ability>> abilityQueue;
+	private Ability lastAbility;
+	private Ability currentAbility;
+
+	private const string CSV_PATH = "Assets/CSV/BossTimeline.csv";
+
+	private void Awake()
+	{
+
+	}
+
+	void Start()
     {
-        
-    }
+		//abilityQueue.Enqueue(new KeyValuePair<float, Ability>(10f, elementalCharge));
+		ReadCSV();
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+	}
+
+	private void ReadCSV()
+	{
+		StreamReader streamReader = new StreamReader(CSV_PATH);
+		bool endOfFile = false;
+		while (!endOfFile)
+		{
+			string data_string = streamReader.ReadLine();
+			if (data_string == null)
+			{
+				endOfFile = true;
+				break;
+			}
+			var data_values = data_string.Split(',');
+			Debug.Log(data_values[0].ToString() + " " + data_values[1].ToString());
+		}
+	}
 }
