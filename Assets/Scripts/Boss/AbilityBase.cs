6ba49d6c72291131fca8104940d5695dacd766b2
ShiroKatake <tgkhang262@gmail.com>
2021-02-21 11:03:09 +1100

    UPDATE: Changed base code

--- Assets/Scripts/Boss/AbilityBase.cs
+++ Assets/Scripts/Boss/AbilityBase.cs
@@ -4,28 +4,12 @@ using UnityEngine;
 
 public abstract class AbilityBase : MonoBehaviour
 {
-	[SerializeField] private float abilityDuration;
+	[SerializeField] protected float abilityDuration;
 
 	protected bool isUsing;
-	protected abstract void Ability();
+	protected float timePassed;
 
-	float timePassed;
-
-	// Update is called once per frame
-	private void Update()
-    {
-		if (timePassed < abilityDuration)
-			timePassed += Time.deltaTime;
-		else
-			isUsing = false;
-
-		if (isUsing)
-		{
-			Ability();
-		}
-	}
-
-	public void Execute()
+	public virtual void Execute(int element = 0)
 	{
 		isUsing = true;
 		timePassed = 0f;
